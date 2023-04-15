68c2af217814230e0961f4328a3cac0b682add26
seonb2n <626ksb@gmail.com>
2023-04-15 22:58:28 +0900

    - 구글 스프레드 시트에 텍스트 커맨드 업로드

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ from PIL import Image
 import requests
 import requests as r
 import wget
+from googleapiclient.http import MediaFileUpload
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import gspread
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+import io
+from PIL import Image
 
 HUGGING_FACE_ACCESS_TONE = ""
 API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+FOLDER_ID = ""
 
 def setChromeDriver():
     url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
@@ -35,12 +43,24 @@ if __name__ == '__main__':
     with (open(secret_file)) as f:
         secrets = json.loads(f.read())
     HUGGING_FACE_ACCESS_TONE = secrets['HUGGING_FACE_TOKEN']
+    FOLDER_ID = secrets["FOLDER_ID"]
     # print(">>> HG 토큰 : " + HUGGING_FACE_ACCESS_TONE)
     setChromeDriver()
 
     command_file = os.path.join("./", "IMAGE_COMMAND.txt")
     file = open(command_file, 'r')
     Lines = file.readlines()
+
+    # 구글 스프레드시트
+    json_key_path = "./secrets.json"  # JSON Key File Path
+    gc = gspread.service_account(filename=json_key_path)
+    doc = gc.open("text2image_test")
+    sheet = doc.worksheet("0415")
+
+    # 구글 드라이브
+    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
 
     count = 0
     for line in Lines :
@@ -50,4 +70,17 @@ if __name__ == '__main__':
         })
         image = Image.open(io.BytesIO(image_bytes))
         count += 1
-        image.save("test{}.jpg".format(count))
+        # image.save("test{}.jpg".format(count))
+        image_base64 = base64.b64encode(image_bytes).decode()
+        sheet.update_acell('A{}'.format(count), line)
+
+        creds = Credentials.from_authorized_user_file('secrets.json', SCOPES)
+        # 이미지 업로드
+        # service = build('drive', 'v3', credentials=creds)
+        # file_metadata = {'name': 'image{}.jpg'.format(count), 'parents': [FOLDER_ID]}
+        # media = MediaFileUpload(image, mimetype='image/jpeg')
+        # file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        # print('File ID: %s' % file.get('id'))
+
+
+
