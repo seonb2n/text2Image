c8fbbfa8441bf4b213a123d737c9ad434506ad1f
seonb2n <626ksb@gmail.com>
2023-04-16 15:21:33 +0900

    - 구글 스프레드 시트에 자동으로 요청 텍스트 와 사진 업로드

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import base64
 import json
 import os
-import time
 import zipfile
-from typing import List
-import io
-from PIL import Image
 import requests
-import requests as r
 import wget
-from googleapiclient.http import MediaFileUpload
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import gspread
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from httplib2 import Http
+from oauth2client import file
 from googleapiclient.errors import HttpError
-from google.oauth2.credentials import Credentials
+
 import io
 from PIL import Image
 
 HUGGING_FACE_ACCESS_TONE = ""
 API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
 FOLDER_ID = ""
+
 
 def setChromeDriver():
     url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
@@ -33,13 +30,15 @@ def setChromeDriver():
         zip_ref.extractall(os.getcwd())
     os.remove(latest_driver_zip)
 
+
 def getImage(payload):
     headers = {"Authorization": "Bearer " + HUGGING_FACE_ACCESS_TONE}
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.content
 
+
 if __name__ == '__main__':
-    secret_file = os.path.join("./", 'secrets.json')
+    secret_file = os.path.join("./", 'secrets_gs.json')
     with (open(secret_file)) as f:
         secrets = json.loads(f.read())
     HUGGING_FACE_ACCESS_TONE = secrets['HUGGING_FACE_TOKEN']
@@ -48,39 +47,46 @@ if __name__ == '__main__':
     setChromeDriver()
 
     command_file = os.path.join("./", "IMAGE_COMMAND.txt")
-    file = open(command_file, 'r')
-    Lines = file.readlines()
+    opendFile = open(command_file, 'r')
+    Lines = opendFile.readlines()
 
     # 구글 스프레드시트
-    json_key_path = "./secrets.json"  # JSON Key File Path
+    json_key_path = "secrets_gs.json"  # JSON Key File Path
     gc = gspread.service_account(filename=json_key_path)
     doc = gc.open("text2image_test")
     sheet = doc.worksheet("0415")
 
     # 구글 드라이브
-    creds = None
-    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    credentials = service_account.Credentials.from_service_account_file('secrets_gs.json')
+    drive_service = build('drive', 'v3', credentials=credentials)
 
+    # 폴더 목록을 가져옵니다.
+    # try:
+    #     folder_query = "mimeType='application/vnd.google-apps.folder' and trashed=false"
+    #     folders = drive_service.files().list(q=folder_query).execute().get('files', [])
+    #     for folder in folders:
+    #         print(f'Folder Name: {folder.get("name")}, Folder ID: {folder.get("id")}')
+    # except HttpError as error:
+    #     print(f'An error occurred: {error}')
 
     count = 0
-    for line in Lines :
+    for line in Lines:
         print("Line: {}".format(line.strip()))
         image_bytes = getImage({
             "inputs": line.strip(),
         })
         image = Image.open(io.BytesIO(image_bytes))
         count += 1
-        # image.save("test{}.jpg".format(count))
-        image_base64 = base64.b64encode(image_bytes).decode()
-        sheet.update_acell('A{}'.format(count), line)
+        file_paths = "result_IMG/test{}.jpg".format(count)
+        image.save(file_paths)
 
-        creds = Credentials.from_authorized_user_file('secrets.json', SCOPES)
         # 이미지 업로드
         # service = build('drive', 'v3', credentials=creds)
-        # file_metadata = {'name': 'image{}.jpg'.format(count), 'parents': [FOLDER_ID]}
-        # media = MediaFileUpload(image, mimetype='image/jpeg')
-        # file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-        # print('File ID: %s' % file.get('id'))
-
-
-
+        file_metadata = {'name': "test{}.jpg".format(count), 'parents': [FOLDER_ID]}
+        media = MediaFileUpload(file_paths, mimetype='image/jpg')
+        uploaded_file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print('File ID: %s' % uploaded_file.get('id'))
+        image_url = f'https://drive.google.com/uc?id={uploaded_file.get("id")}'
+        image_formula = f'=IMAGE("{image_url}")'
+        sheet.update_acell('A{}'.format(count), line)
+        sheet.update_acell('B{}'.format(count), image_formula)
